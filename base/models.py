85011427f05dd5e0a6da1bda610b43cd445d39b7
Aslan <asl.zurabov@gmail.com>
2022-01-09 16:06:34 +0300

    dd

--- base/models.py
+++ base/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.fields import TextField
 
 class User(AbstractUser):
-    pass 
+    name = models.CharField(max_length=200,null=True)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(null=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []    
