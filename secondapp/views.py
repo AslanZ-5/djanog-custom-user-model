3e582a9132464f0ed6447b8b0397cb964b9b00bf
Aslan <asl.zurabov@gmail.com>
2022-01-23 20:01:25 +0300

    admin permissions

--- secondapp/views.py
+++ secondapp/views.py
@@ -6,4 +6,4 @@ from django.views import View
 class Myview(View):
     def get(self, request, *args, **kwargs):
         return HttpResponse('Helllow world')
-    def dispatch(self, request, *args, **kwargs):
+
