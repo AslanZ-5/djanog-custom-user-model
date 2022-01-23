82bb9cba772337269d44df7ed54e10bda235bd1c
Aslan <asl.zurabov@gmail.com>
2022-01-23 20:19:09 +0300

    ModelAdmin.has_add_permission and has_change_permission()

--- base/admin.py
+++ base/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
 from .models import Comment, Post
+
+
+class BlogAdminArea(admin.AdminSite):
+    site_header = 'Blog database'
+
+
+class TestAdminPermissions(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+blog_site = BlogAdminArea(name='BlogAdmin')
+
+blog_site.register(Post, TestAdminPermissions)
+
 admin.site.register(Post)
 admin.site.register(Comment)
--- usermodel/urls.py
+++ usermodel/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from base.admin import  blog_site
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog-admin/',blog_site.urls),
     path('',include('base.urls')),
     path('s/',include('secondapp.urls')),
 ]
